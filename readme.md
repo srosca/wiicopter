9bc2a6a6d1dbc946afeeeeef3542723a2d928efc
Paul Leader <paul@paulleader.co.uk>
2013-03-24 15:02:28 +0000

    Tidy up and document

--- readme.md
+++ readme.md
@@ -1,15 +1,35 @@
-Drive the Monster Milktruck (or Segway) with the Wii Fit Balance Board.
+## Intro
 
-Video of app in use at SVI Hackathon:
-http://www.youtube.com/watch?v=Jnsxsn2CZFo
+Control a [NodeCopter](http://nodecopter.com) using the Wii Fit Balance Board and a Wii Remote.
 
-Wii Fit Balance Board -> [Osculator](http://www.osculator.net) -> this (node server.js)
+Wii Fit Balance Board/Wii Remote -> [Osculator](http://www.osculator.net) -> This (node wiicopter.js) -> NodeCopter
 
-Writeup:
-http://www.quora.com/Stormy-Shippy/Virtual-Segway-tours-with-a-Wii-Fit-balance-board
+## Connecting to the Controllers
 
-adapted from:
+To get the data from the controllers I've used [Osculator](http://www.osculator.net) for OS X. The file WiiCopter.oscd is a configuration that should work.
 
+## Controlling Your Copter
+
+### Launching
+
+Stand on the board and press the A button once, while holding the Wii Remote level and stationary. The copter will rise to the default start height.
+
+### Controlling
+
+Leaning on the balance board controls forward/backward/left/right (relative to the camera direction).
+
+Rotating the Wii Remote along its long axis controls the rotation of the copter, rotate left to rotate the copter counter-clockwise, and right for clockwise.
+
+The pitch of the Wii Remote raises or lowers the copter. The copter will rise or fall in proportion to the angle of the Wii Remote from the horizontal. Don't point the remote straight up or down as it sometimes seems to go a bit mad and shoots up too fast. Don't make violent movements with the Wii Remote.
+
+Start out by making small movements with the board, and then try making small movements with the Wii Remote before working your way up to more complex maneuvers.
+
+### Emergency Stop
+
+If you need to stop the copter at any point simply jump off the Wii Balance Board: the copter will immediately halt and execute a controlled landing.  You should be able to restart the copter by standing back on the board and pressing A.
+
+## Adapted from:
+
+* https://github.com/levity/svi-balance-board
 * http://github.com/catshirt/node-hands
 * http://github.com/automata/node-osc
-* http://earth-api-samples.googlecode.com/svn/trunk/demos/milktruck/index.html
---
+++ wiicopter.js
@@ -0,0 +1,128 @@
+var connect = require('connect'),
+  fs = require('fs'),
+  socketIO = require('socket.io'),
+  jspack = require('./lib/node-jspack/jspack').jspack,
+  express = require('express'),
+  osc = require('./lib/osc');
+
+var arDrone = require('ar-drone');
+var client;
+
+// Prepare listening socket for the OSC data
+var dgram = require('dgram');
+osc_serv = dgram.createSocket('udp4');
+
+
+// State global for the control data
+var state = {sum: 0, 
+             x: 0.5,
+             y: 0.5,
+             go: true,
+             a: false,
+             b: false,
+             pitch: 0.5,
+             roll: 0.5};
+
+// Track the state of the copter.
+// Obviously this only tracks the state we *think* 
+// the copter is in, it doesn't take into account crashes.
+var flying = false;
+
+// Utility function to round control values to N decimal places.
+function smooth(val, dp) {
+  m = Math.pow(10, dp);
+  return Math.round(val * m)/m;
+}
+
+function launch_copter() {
+  // Start up drone client
+  client = arDrone.createClient();
+  client.disableEmergency();
+}
+
+
+function run_cmds(s) {
+  console.log(s);
+
+  if (s.a) {
+    // Press the A button to take off.
+    client.disableEmergency();
+    client.takeoff();
+    flying = true;
+  } else if (s.b) {
+    // Press the B (trigger) button to do a flip
+    client.animate('flipAhead', 1500);
+  } else if (!s.fly && flying) {
+    // Emergency cut out.
+    // Stop the copter and land it.
+    client.stop();
+    client.land();
+    flying = false;
+  } else {
+    // Just grab the control data and send commands
+    // Data from the Wii is 0 to 1 with 0.5 being neutral position
+    // We subtract 0.5 to make it work for the nodecopter.
+    // Note that this approach also reduces the sensitivity, making
+    // the copter a bit slower but easier to fly.
+    // We could easily map directy on a 1 for 1 scale, but would probably
+    // make it quite twitchy.
+    client.clockwise(s.roll - 0.5);
+    client.front(s.y - 0.5);
+    client.right(s.x - 0.5);
+    client.up(s.pitch - 0.5);
+  }
+}
+
+
+// Event handler for the data received from the Wii via OSCulator
+osc_serv.on('message', function (msg, a) {
+  var val = osc.decode(msg);
+  
+  // The first item in the val array is the device
+  // e.g "/wii/2/balance" is the balance board
+  // The remaining data is the variables from the controller.
+  field = val[0];
+  controller = field.split("/").pop(); // Which controller?
+  
+  switch(controller) {
+    case 'balance':
+      // Get the X/Y postion of the CoG on the Balance Board
+      state.x = smooth(val[6], 1);
+      state.y = smooth(val[7], 1);
+      
+      // Get the "sum" value for the balance board
+      state.sum = val[5];
+      if (state.sum < 0.2)
+        // There's nobody on the board, so stop the copter
+        state.go = false;
+      else
+        state.go = true;
+      break;
+    case 'A':
+      if (val[1] == 1)
+        state.a = true;
+      else
+        state.a = false;
+      break;
+    case 'B':
+      if (val[1] == 1)
+        state.b = true;
+      else
+        state.b = false;  
+      break;
+    case 'pry':
+      // This is the accelerometer data from the WiiMote
+      state.pitch = smooth(val[1], 1);
+      state.roll  = smooth(val[2], 1);
+      break;
+  }
+  
+  // Having updated the state we send the relevant commands
+  // to the copter.
+  run_cmds(state);
+})
+
+// listen for incoming messages from Osculator client
+// be sure to set port and IP address to where your
+// Osculator client routes OSC messages.
+binding = osc_serv.bind(9000, '127.0.0.1');
